3d53e7adac9e593ebccdd266e879f6966ac2f394
alexbrentcode <alex.brent.code@gmail.com>
2023-04-08 13:58:15 +0100

    Refactored image upload and added dispaly

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useEffect, useState } from 'react';
 import './App.css'
-import exifr, { parse } from 'exifr';
+import Homepage from './pages/Homepage';
 
 function App() {  
-  const [uploadedImage, setUploadedImage] = useState<any>();
-  const [imageURL, setImageUrl] = useState<any>();
-
-  const processImage = async (image: any) => {
-    const fileUrl = window.URL.createObjectURL(image.target.files![0]);
-    setImageUrl(fileUrl);
-    setUploadedImage(image);
-    let {latitude, longitude} = await exifr.gps(fileUrl)
-
-    console.log(`${latitude}, ${longitude}`)
-  }
 
   return (
     <div className="App">
       <div style={{display: 'flex', width: '100vw', height: '100vh', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-        <div style={{border: '1px solid black', width: 480, height: 480, flexDirection: 'inherit', display: 'flex', alignItems: 'inherit', justifyContent: 'inherit'}}>
-          <h2>Upload your image</h2>
-          <input type='file' name='imageTest' onChange={(e) => {processImage(e)}} />
-          <img src={imageURL}style={{width: 240, height: 240, border: '1px solid black'}}/>
-          <pre id="allMetaDataSpan"></pre>
-        </div>
-
+        <Homepage />
       </div>
     </div>
   )
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+import exifr from "exifr";
+
+interface CoordinateInterface {
+    latitude: number,
+    longitude: number
+}
+
+const Homepage = () => {
+    const [uploadedImage, setUploadedImage] = useState<any>();
+    const [imageURL, setImageUrl] = useState<any>();
+    const [coordinates, setCoordinates] = useState<CoordinateInterface>();
+    const processImage = async (image: any) => {
+      const fileUrl = window.URL.createObjectURL(image.target.files![0]);
+      setImageUrl(fileUrl);
+      setUploadedImage(image);
+      let {latitude, longitude} = await exifr.gps(fileUrl)
+      setCoordinates({latitude: latitude, longitude: longitude})
+      
+    }
+
+    useEffect(() => {
+        console.log(`Coordinates are ${JSON.stringify(coordinates)}`)  
+    })
+  
+    return (
+        <>
+        <div style={{border: '1px solid black', width: 480, height: 480, flexDirection: 'inherit', display: 'flex', alignItems: 'inherit', justifyContent: 'inherit'}}>
+          <h2>Upload your image</h2>
+          <input type='file' name='imageTest' onChange={(e) => {processImage(e)}} />
+          <img src={imageURL}style={{width: 240, height: 240, border: '1px solid black'}}/>
+          <div>
+            {coordinates &&
+            <>
+            <p>Lat: {coordinates?.latitude}</p>
+            <p>Long: {coordinates?.longitude} </p>
+            </>
+            }
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Homepage
