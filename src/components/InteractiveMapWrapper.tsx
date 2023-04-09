bf6fb6b5c207b6bf94c88e6cd173a3ae04ce2de9
alexbrentcode <alex.brent.code@gmail.com>
2023-04-09 17:01:11 +0100

    Added logic for finding central coordiants of images uploaded

--- src/components/InteractiveMapWrapper.tsx
+++ src/components/InteractiveMapWrapper.tsx
@@ -7,17 +7,42 @@ const InteractiveMapWrapper:FC<InteractiveMapWrapperInterface> = ({imageMetaData
         lat: 51.513974,
         lng: -0.030228
     }
-    //Currently map central point will be first coordinate
-    //Eventual goal, calculate midpoint of all coordinates passed and scale of zoom based on total distances
+
+    //Mean of 5 testImages form The Great Gable
+    //MID LAT 54.4880875
+    //MID LGN -3.210903472222222
+    //metersPerPx = 156543.03392 * Math.cos(latLng.lat() * Math.PI / 180) / Math.pow(2, zoom)
+
     const [mapCentralPoint, setMapCentralPoint] = useState<GoogleMapsCoordinates>(initialState)
+
+
+    const calculateMidPointOfAllCoordinates = (imageMetaData: any) => {
+        //Assign totals...
+        let [latitudeCount, longitudeCount] = [imageMetaData.length, imageMetaData.length]
+        let [latitudeMean, longitudeMean] = [0,0];
+    
+        imageMetaData.forEach((imageCoord: any) => {
+            isNaN(imageCoord.latitude) ? latitudeCount-- : latitudeMean += imageCoord.latitude
+            isNaN(imageCoord.longitude) ? longitudeCount-- : longitudeMean += imageCoord.longitude
+        })
+
+        //Return to 15th decimal as it's the max degree of accuracy recorded in coordiantes
+        return {meanLat: Number((latitudeMean/ latitudeCount).toFixed(15)), meanLng: Number((longitudeMean/ longitudeCount).toFixed(15))}
+    }
+
 
     useEffect(() => {
         if(imageMetaData.length > 0){
-            console.log(`Incoming lat ${imageMetaData[0].latitude}, ${imageMetaData[0].latitude}`)
-            setMapCentralPoint({
-                lat: Number(imageMetaData[0].latitude),
-                lng: Number(imageMetaData[0].longitude)
-            })
+            let {meanLat, meanLng} = calculateMidPointOfAllCoordinates(imageMetaData);
+            console.log('Final calculated values are lat ' + meanLat + ' / lng ' + meanLng)
+
+            //If numbers are valid, assign to centralPoint
+            if(typeof meanLat === 'number' && typeof meanLng === 'number'){
+                setMapCentralPoint({
+                    lat: meanLat,
+                    lng: meanLng
+                })
+            }
         }
 
     }, [imageMetaData])
